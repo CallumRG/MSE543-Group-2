c171fe796964e23994ef77d776a3102fc773c491
Callum Gillies <callum.r.gillies@gmail.com>
2025-06-13 20:10:43 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/recipe/[id]/page.tsx
+++ src/app/recipe/[id]/page.tsx
@@ -111,12 +111,6 @@ export default async function RecipePage({ params }: RecipePageProps) {
           </Button>
         </CardFooter>
       </Card>
-      
-      <style jsx global>{`
-        .shadow-text {
-          text-shadow: 1px 1px 3px rgba(0,0,0,0.7);
-        }
-      `}</style>
     </article>
   );
 }
